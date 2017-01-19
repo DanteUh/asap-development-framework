56b1edeabed38b04fe67aac163644ecf051f08a2
attilac <attila.cederbygd@gmail.com>
2017-01-19 09:24:32 +0100

    wrapped nav.js in jquery document ready function

--- js/nav.js
+++ js/nav.js
@@ -1,18 +1,26 @@
 //JS for navbar 
-
-//glob vars
-window.addEventListener('scroll', navbarBg);
-
-
-//navbar background change
-function navbarBg() {
-    var viewPort = document.getElementsByTagName('header')[0];
-    var navBar = document.getElementById('siteNav');
-    if (document.body.scrollTop > (60) || document.documentElement.scrollTop > (60)) {
-    	navBar.classList.remove('bg-transparent');
-        navBar.classList.add('bg-primary') ;
-    } else {
-        navBar.classList.remove('bg-primary');
-        navBar.classList.add('bg-transparent') ;
-    }
+if (typeof jQuery === 'undefined') {
+  throw new Error('This js-file requires jQuery');
 }
+
+(function ($){
+	'use strict';
+    var $window = $(window);
+
+	//glob vars
+	window.addEventListener('scroll', navbarBg);
+
+	//navbar background change
+	function navbarBg() {
+	    var viewPort = document.getElementsByTagName('header')[0];
+	    var navBar = document.getElementById('siteNav');
+	    if (document.body.scrollTop > (60) || document.documentElement.scrollTop > (60)) {
+	    	navBar.classList.remove('bg-transparent');
+	        navBar.classList.add('bg-primary') ;
+	    } else {
+	        navBar.classList.remove('bg-primary');
+	        navBar.classList.add('bg-transparent') ;
+	    }
+	}
+
+}(jQuery));
