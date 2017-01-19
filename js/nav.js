adfb6b666996b66c77d62f188bf2fdccbf439b1b
attilac <attila.cederbygd@gmail.com>
2017-01-19 07:55:27 +0100

    Cleanup on all pages, added basic navbar scroll bg-change, formcontainer padding fix, autoprefixer, button-styling in footer, form-input sizing, nav-lnínks should be fixed

--- js/nav.js
+++ js/nav.js
@@ -1,16 +1,18 @@
 //JS for navbar 
 
 //glob vars
-window.addEventListener("scroll", navbarBg);
+window.addEventListener('scroll', navbarBg);
 
 
 //navbar background change
 function navbarBg() {
-    var viewPort = document.getElementsByTagName("header")[0];
-
+    var viewPort = document.getElementsByTagName('header')[0];
+    var navBar = document.getElementById('siteNav');
     if (document.body.scrollTop > (60) || document.documentElement.scrollTop > (60)) {
-        document.getElementById("siteNav").className = "bg-blue";
+    	navBar.classList.remove('bg-transparent');
+        navBar.classList.add('bg-primary') ;
     } else {
-        document.getElementById("siteNav").className = "";
+        navBar.classList.remove('bg-primary');
+        navBar.classList.add('bg-transparent') ;
     }
 }
