edd0ffaff2f40c39b81c5a30e304d05c9147a6a7
attilac <attila.cederbygd@gmail.com>
2017-01-21 00:13:48 +0100

    index animation mystery solved, bootsrtap hamburger is a span

--- js/index-animate.js
+++ js/index-animate.js
@@ -1,7 +1,8 @@
 // ANIMATE INDEX HEADLINE
 
 window.addEventListener("DOMContentLoaded", interval, false);
-var indexHeader = document.getElementsByTagName("span");
+var header = document.getElementById("index-bg");
+var indexHeader = header.getElementsByTagName("span");
 var largeFlash = document.getElementById("large-flash");
 var smallFlash = document.getElementById("small-flash");
 
@@ -10,7 +11,7 @@ var timesRun = 0;
 var interval = setInterval(function() {
     indexHeader[timesRun].setAttribute("class", "white-text");
     timesRun++;
-    if (timesRun === 4) {
+    if (timesRun === 3) {
         clearInterval(interval);
         displayFlash();
     }
@@ -20,7 +21,7 @@ var interval = setInterval(function() {
 function displayFlash() {
     largeFlash.setAttribute("class", "flash-after");
     largeFlash.setAttribute("class", "align-self-center");
-    smallFlash.classList.add('flash-after');
+    smallFlash.setAttribute("class", "flash-after");
 }
 
 
