95c0c51d2c3ffe6b76664d745f86e268df013dad
attilac <attila.cederbygd@gmail.com>
2017-01-21 13:58:38 +0100

    responsive navbar in blue, hamburger get swapped with close icon on mobile when collapsed. Changes in nav.js, it checkes if scrolltop is < 60 it swaps to transparent background

--- js/nav.js
+++ js/nav.js
@@ -9,18 +9,37 @@ if (typeof jQuery === 'undefined') {
 
 	//glob vars
 	window.addEventListener('scroll', navbarBg);
+	var navBar = document.getElementById('siteNav');
 
 	//navbar background change
 	function navbarBg() {
-	    var viewPort = document.getElementsByTagName('header')[0];
-	    var navBar = document.getElementById('siteNav');
-	    if (document.body.scrollTop > (60) || document.documentElement.scrollTop > (60)) {
-	    	navBar.classList.remove('bg-transparent');
-	        navBar.classList.add('bg-primary') ;
-	    } else {
-	        navBar.classList.remove('bg-primary');
-	        navBar.classList.add('bg-transparent') ;
-	    }
+		if( ! $('#mainNavigation').hasClass('show')) {
+		    var viewPort = document.getElementsByTagName('header')[0];
+		    if (document.body.scrollTop > (60) || document.documentElement.scrollTop > (60)) {
+		    	navBar.classList.remove('bg-transparent');
+		        navBar.classList.add('bg-primary') ;
+		    } else {
+		        navBar.classList.remove('bg-primary');
+		        navBar.classList.add('bg-transparent') ;
+		    }
+		}
 	}
+
+	$('#mainNavigation').on('show.bs.collapse', function () {
+		navBar.classList.remove('bg-transparent');
+  		navBar.classList.add('bg-primary');		
+  		$('.navbar-toggler-icon').hide();
+  		$('.nav-close').show();
+	});
+
+	$('#mainNavigation').on('hidden.bs.collapse', function () {
+		if (document.body.scrollTop < (60)) {
+        	navBar.classList.remove('bg-primary');
+        	navBar.classList.add('bg-transparent') ;	
+        }
+        	$('.nav-close').hide();
+  			$('.navbar-toggler-icon').show();
+	});
+
 
 }(jQuery));
